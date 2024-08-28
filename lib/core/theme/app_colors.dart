295075d097879e2bc2c6855f6462c6f03ca47fd7
ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-08-28 05:39:56 +0300

    Create App Colors

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors
+{
+  static const Color primaryColor = Color(0xFF42C83C);
+  static const Color lightBackgroundColor = Color(0xFFF2F2F2);
+  static const Color darkBackgroundColor = Color(0xFF0D0C0C);
+  static const Color greyColor = Color(0xFFBEBEBE);
+  static const Color darkGreyColor = Color(0xFF343434);
+}
