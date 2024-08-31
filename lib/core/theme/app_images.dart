8c919ecca4d34dd68fb2cb13517b1f4f0b1690a9
ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-08-31 04:58:43 +0300

    Add Images

---
+++ lib/core/theme/app_images.dart
@@ -0,0 +1,8 @@
+class AppImages {
+  static const String basePath = "assets/images/";
+
+
+  static String getStartedBG = "${basePath}get_started_bg.png";
+  static String chooseModeBG = "${basePath}choose_mode_bg.png";
+
+}
