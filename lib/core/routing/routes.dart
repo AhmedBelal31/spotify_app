8f5a074f164720e4f814b4221023d46478983951
ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-08-31 04:36:04 +0300

    Add New Routes

--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -1,5 +1,7 @@
 class Routes {
   static const String splashPage = '/splashPage';
+  static const String getStartedPage = '/getStartedPage';
+  static const String chooseModePage = '/chooseModePage';
   static const String loginPage = '/loginPage';
   static const String homePage = '/homePage';
 }
