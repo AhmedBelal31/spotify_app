6ab08d96bbbb8e498e7fb798ec69b50df3a06c4a
ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-08-31 04:59:11 +0300

    Add Images/Vectors

--- lib/core/theme/app_vectors.dart
+++ lib/core/theme/app_vectors.dart
@@ -1,6 +1,7 @@
 class AppVectors {
   static const String basePath = "assets/svgs/";
 
-
   static String appLogo = "${basePath}app_logo.svg";
+  static String chooseModeMoon = "${basePath}moon.svg";
+  static String chooseModeSun = "${basePath}sun.svg";
 }
