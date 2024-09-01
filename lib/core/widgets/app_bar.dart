74b2d075b76579feb902b9e3521c14c3fa5d582c
ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-09-01 08:14:45 +0300

    Create AppBar

---
+++ lib/core/widgets/app_bar.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:spotify_app/core/extensions/is_dark_mode.dart';
+
+class BasicAppBar extends StatelessWidget {
+  const BasicAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      scrolledUnderElevation: 0,
+      leading: IconButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        icon: Container(
+          width: 50,
+          height: 50,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: context.isDarkMode
+                ? Colors.white.withOpacity(.07)
+                : Colors.black.withOpacity(.04),
+          ),
+          child: Icon(
+            Icons.arrow_back_ios_new,
+            size: 15,
+            color: context.isDarkMode ? Colors.white : Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
