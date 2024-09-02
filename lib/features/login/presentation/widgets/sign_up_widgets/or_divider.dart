d6064315a45ec95fe2652c43ea0f18f7d7c02e70
ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-09-02 12:10:04 +0300

    Create Or Divider

---
+++ lib/features/login/presentation/widgets/sign_up_widgets/or_divider.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import '../../../../../core/theme/app_text_styles.dart';
+
+class OrDivider extends StatelessWidget {
+  const OrDivider({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Container(
+            height: 1,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.grey[700]!,
+                  Colors.grey[500]!,
+                  Colors.grey[200]!,
+                ],
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+              ),
+            ),
+          ),
+        ),
+         Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8),
+          child: Text(
+              'Or',
+              style: TextStyles.font15Regular.copyWith(
+                color: Colors.grey
+              )
+          ),
+        ),
+        Expanded(
+          child: Container(
+            height: 1,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  // Color(0xFF5B5B5B),
+                  // Color(0xFF252525),
+                  Colors.grey[700]!,
+                  Colors.grey[500]!,
+                  Colors.grey[200]!,
+                ],
+                begin: Alignment.centerRight,
+                end: Alignment.centerLeft,
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(width: 10),
+      ],
+    );
+  }
+}
