8d9ab2cf4e6867c369a7eab288446983b04c9c94
ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-09-02 15:31:01 +0300

    Add Firebase To Project

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:spotify_app/core/routing/app_router.dart';
 import 'package:spotify_app/spotify_app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,6 +13,9 @@ Future<void> main() async {
     storageDirectory: kIsWeb
         ? HydratedStorage.webStorageDirectory
         : await getApplicationDocumentsDirectory(),
+  );
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(
     SpotifyApp(appRouter: AppRouter()),
