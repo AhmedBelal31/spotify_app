562d82a32b1555d16ec24c529cb2634f0ae7ee50
ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-09-03 04:39:11 +0300

    Create SignUpWithEmailAndPassword UseCase || Domain Layer

---
+++ lib/features/login/domain/usecases/sign_up_with_email_and_password_usecase.dart
@@ -0,0 +1,12 @@
+import 'package:dartz/dartz.dart';
+import 'package:spotify_app/core/errors/failure.dart';
+import '../entities/user_entity.dart';
+import '../repo/auth_repo.dart';
+
+class SignUpWithEmailAndPasswordUseCase {
+  final AuthRepo authRepo;
+  SignUpWithEmailAndPasswordUseCase({required this.authRepo});
+  Future<Either<Failure, UserEntity>> call({required String name,required String email ,required String password}) async {
+    return await authRepo.signUpWithEmailAndPassword(name: name, email: email, password: password);
+  }
+}
