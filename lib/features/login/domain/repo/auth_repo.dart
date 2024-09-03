0c9adfed6071ebcf9f66b509516e39e52128ccf0
ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-09-03 04:40:10 +0300

    Create Auth Repo || Domain Layer

---
+++ lib/features/login/domain/repo/auth_repo.dart
@@ -0,0 +1,16 @@
+import 'package:spotify_app/features/login/domain/entities/user_entity.dart';
+import 'package:dartz/dartz.dart';
+import '../../../../core/errors/failure.dart';
+
+abstract class AuthRepo {
+  Future<Either<Failure, UserEntity>> signUpWithEmailAndPassword({
+    required String name,
+    required String email,
+    required String password,
+  });
+
+  Future<Either<Failure, UserEntity>> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  });
+}
