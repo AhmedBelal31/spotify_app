60156180f7d31909d164e4ab534bee4b19d70d49
ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-09-04 16:42:04 +0300

    Add SignIn Cubit To Get_It

--- lib/core/di/get_it.dart
+++ lib/core/di/get_it.dart
@@ -1,8 +1,10 @@
 import 'package:get_it/get_it.dart';
 import 'package:spotify_app/features/login/domain/repo/auth_repo.dart';
+import 'package:spotify_app/features/login/domain/usecases/sign_in_with_email_password_usecase.dart';
 import 'package:spotify_app/features/login/domain/usecases/sign_up_with_email_and_password_usecase.dart';
+import 'package:spotify_app/features/login/presentation/cubit/sign_in_cubit/sign_in_cubit.dart';
 import '../../features/login/data/auth_repo_impl/auth_repo_impl.dart';
-import '../../features/login/presentation/sign_up_cubit/sign_up_cubit.dart';
+import '../../features/login/presentation/cubit/sign_up_cubit/sign_up_cubit.dart';
 
 class ServiceLocator {
   static GetIt di = GetIt.instance;
@@ -19,5 +21,14 @@ class ServiceLocator {
     di.registerFactory(() => SignUpCubit(
         signUpWithEmailAndPasswordUseCase:
             di<SignUpWithEmailAndPasswordUseCase>()));
+
+    //register SignIn UseCase
+    di.registerLazySingleton<SignInWithEmailAndPasswordUseCase>(
+        () => SignInWithEmailAndPasswordUseCase(authRepo: di<AuthRepo>()));
+
+    //Register SignUp Cubit
+    di.registerFactory(() => SignInCubit(
+        signInWithEmailAndPasswordUseCase:
+            di<SignInWithEmailAndPasswordUseCase>()));
   }
 }
