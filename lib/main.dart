600754fc847e5ef3dc257579a2ca3cd9082fe16e
ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-09-05 07:16:45 +0300

    Create Bloc Observer

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,13 @@ import 'package:path_provider/path_provider.dart';
 import 'package:spotify_app/core/routing/app_router.dart';
 import 'package:spotify_app/spotify_app.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'core/di/get_it.dart';
+import 'core/network/bloc_observer.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = MyBlocObserver();
   HydratedBloc.storage = await HydratedStorage.build(
     storageDirectory: kIsWeb
         ? HydratedStorage.webStorageDirectory
@@ -17,6 +20,7 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  ServiceLocator.initGetIt();
   runApp(
     SpotifyApp(appRouter: AppRouter()),
   );
