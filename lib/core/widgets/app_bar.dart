d3a0b1ab9192e20c07cb66810569d9ec7d789f37
ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-09-12 15:04:48 +0300

    Edit On App TabBar

--- lib/core/widgets/app_bar.dart
+++ lib/core/widgets/app_bar.dart
@@ -5,12 +5,20 @@ import 'package:spotify_app/core/theme/app_vectors.dart';
 import '../theme/app_text_styles.dart';
 
 class BasicAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final bool? showBackButton;
   final bool withLogo;
   final String text;
   final Widget? trailing;
+  final Widget? leading;
 
-  const BasicAppBar(
-      {super.key, this.withLogo = false, this.text = '', this.trailing});
+  const BasicAppBar({
+    super.key,
+    this.showBackButton = true,
+    this.withLogo = false,
+    this.text = '',
+    this.trailing,
+    this.leading ,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -18,28 +26,32 @@ class BasicAppBar extends StatelessWidget implements PreferredSizeWidget {
       backgroundColor: Colors.transparent,
       elevation: 0,
       scrolledUnderElevation: 0,
-      leading: IconButton(
-        onPressed: () {
-          Navigator.pop(context);
-        },
-        icon: Container(
-          width: 35,
-          height: 35,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: context.isDarkMode
-                ? Colors.white.withOpacity(.07)
-                : Colors.black.withOpacity(.04),
-          ),
-          child: Icon(
-            Icons.arrow_back_ios_new,
-            size: 15,
-            color: context.isDarkMode ? Colors.white : Colors.black,
-          ),
-        ),
-      ),
+      leading: showBackButton == false
+          ?  leading
+          : IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: Container(
+                width: 35,
+                height: 35,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: context.isDarkMode
+                      ? Colors.white.withOpacity(.07)
+                      : Colors.black.withOpacity(.04),
+                ),
+                child: Icon(
+                  Icons.arrow_back_ios_new,
+                  size: 15,
+                  color: context.isDarkMode ? Colors.white : Colors.black,
+                ),
+              ),
+            ),
+      //width :108 ,
+      //height : 40 ,
       title: withLogo
-          ? SvgPicture.asset(AppVectors.appLogo, width: 108)
+          ? SvgPicture.asset(AppVectors.appLogo, width: 108 , height: 33,)
           : Text(
               text,
               style: TextStyles.font16Bold,
@@ -53,6 +65,5 @@ class BasicAppBar extends StatelessWidget implements PreferredSizeWidget {
   }
 
   @override
-  // TODO: implement preferredSize
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
