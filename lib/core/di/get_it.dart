b7cb76d79cc4ab976187a0583218542b02b828e3
ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-09-16 03:27:12 +0300

    Register Home Cubit ->DI

--- lib/core/di/get_it.dart
+++ lib/core/di/get_it.dart
@@ -1,8 +1,12 @@
 import 'package:get_it/get_it.dart';
+import 'package:spotify_app/features/home_page/domain/usecases/get_songs_use_case.dart';
 import 'package:spotify_app/features/login/domain/repo/auth_repo.dart';
 import 'package:spotify_app/features/login/domain/usecases/sign_in_with_email_password_usecase.dart';
 import 'package:spotify_app/features/login/domain/usecases/sign_up_with_email_and_password_usecase.dart';
 import 'package:spotify_app/features/login/presentation/cubit/sign_in_cubit/sign_in_cubit.dart';
+import '../../features/home_page/data/repos_impl/get_songs_repo_impl.dart';
+import '../../features/home_page/domain/repo/get_songs_repo.dart';
+import '../../features/home_page/presentation/cubit/home_cubit/home_cubit.dart';
 import '../../features/login/data/auth_repo_impl/auth_repo_impl.dart';
 import '../../features/login/presentation/cubit/sign_up_cubit/sign_up_cubit.dart';
 
@@ -30,5 +34,15 @@ class ServiceLocator {
     di.registerFactory(() => SignInCubit(
         signInWithEmailAndPasswordUseCase:
             di<SignInWithEmailAndPasswordUseCase>()));
+
+    //Register GetSongs Repo
+    di.registerLazySingleton<GetSongsRepo>(() => GetSongsRepoImpl());
+
+    //Register GetSongs UseCase
+    di.registerLazySingleton<GetSongsUseCase>(
+        () => GetSongsUseCase(getSongsRepo: di<GetSongsRepo>()));
+    //Register Home Cubit
+    di.registerFactory<HomeCubit>(
+        () => HomeCubit(getSongsUseCase: di<GetSongsUseCase>()));
   }
 }
