fa0a5ecabbc4f03617f43a7ed83e454541d50f81
ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-09-16 03:27:57 +0300

    Create Custom Progress Indicator

---
+++ lib/core/widgets/custom_progress_indicator.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:spotify_app/core/theme/app_colors.dart';
+
+class CustomProgressIndicator extends StatelessWidget {
+  const CustomProgressIndicator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: CircularProgressIndicator(
+        color: AppColors.primaryColor,
+      ),
+    );
+  }
+}
