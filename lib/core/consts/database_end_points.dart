ed769bcfea7c3d17188484a9b77667938ff7af02
ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-09-17 20:56:55 +0300

    Add DB URls

--- lib/core/consts/database_end_points.dart
+++ lib/core/consts/database_end_points.dart
@@ -1,2 +1,8 @@
-  const String userCollection = 'users' ;
-  const String songsCollection = 'songs' ;
+const String userCollection = 'users';
+
+const String songsCollection = 'songs';
+
+//https://firebasestorage.googleapis.com/v0/b/spotify-app-31.appspot.com/o/covers%2FBillie_Eilish_Khalid_-_lovely.png?alt=media
+const String firebaseStorage =
+    'https://firebasestorage.googleapis.com/v0/b/spotify-app-31.appspot.com/o/covers%2F';
+const String firebaseStorageMediaAlt ='?alt=media' ;
