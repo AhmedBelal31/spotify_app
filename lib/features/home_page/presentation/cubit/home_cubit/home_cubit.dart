33eea5619e001a5f08d256473b17bf3c3d186a3e
ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-09-17 20:56:12 +0300

    Add  Get PlayList To Home Cubit

--- lib/features/home_page/presentation/cubit/home_cubit/home_cubit.dart
+++ lib/features/home_page/presentation/cubit/home_cubit/home_cubit.dart
@@ -2,16 +2,17 @@ import 'package:bloc/bloc.dart';
 import 'package:dartz/dartz.dart';
 import 'package:meta/meta.dart';
 import 'package:spotify_app/features/home_page/domain/usecases/get_songs_use_case.dart';
-
 import '../../../../../core/errors/failure.dart';
 import '../../../domain/entities/songs_entity.dart';
+import '../../../domain/usecases/get_play_list_use_case.dart';
 
 part 'home_states.dart';
 
 class HomeCubit extends Cubit<HomeStates> {
   GetSongsUseCase getSongsUseCase;
+  GetPlayListUseCase getPlayListUseCase;
 
-  HomeCubit({required this.getSongsUseCase}) : super(HomeInitialState());
+  HomeCubit({required this.getSongsUseCase , required this.getPlayListUseCase}) : super(HomeInitialState());
 
   Future<void> getSongs() async {
     emit(GetSongsLoadingState());
@@ -21,6 +22,18 @@ class HomeCubit extends Cubit<HomeStates> {
       emit(GetSongsFailureState(failure: failure.message));
     }, (songs) {
       emit(GetSongsSuccessState(songs: songs));
+    });
+  }
+
+  Future<void> getPlayList() async {
+    emit(GetPlayListLoadingState());
+
+    Either<Failure, List<SongsEntity>> eitherFailureOrPlayList =
+        await getPlayListUseCase.call();
+    eitherFailureOrPlayList.fold((failure) {
+      emit(GetPlayListFailureState(failure: failure.message));
+    }, (songs) {
+      emit(GetPlayListSuccessState(songs: songs));
     });
   }
 }
