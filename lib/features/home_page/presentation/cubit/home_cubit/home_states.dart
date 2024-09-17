85a2823a39dfa020863bcbe80d88f48403113736
ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-09-17 20:56:20 +0300

    Add  Get PlayList To Home States

--- lib/features/home_page/presentation/cubit/home_cubit/home_states.dart
+++ lib/features/home_page/presentation/cubit/home_cubit/home_states.dart
@@ -5,6 +5,7 @@ sealed class HomeStates {}
 
 final class HomeInitialState extends HomeStates {}
 
+//Get Songs
 final class GetSongsLoadingState extends HomeStates {}
 
 final class GetSongsSuccessState extends HomeStates {
@@ -17,4 +18,19 @@ final class GetSongsFailureState extends HomeStates {
   final String failure;
 
   GetSongsFailureState({required this.failure});
+}
+
+/// Get Play List
+final class GetPlayListLoadingState extends HomeStates {}
+
+final class GetPlayListSuccessState extends HomeStates {
+  final List<SongsEntity> songs;
+
+  GetPlayListSuccessState({required this.songs});
+}
+
+final class GetPlayListFailureState extends HomeStates {
+  final String failure;
+
+  GetPlayListFailureState({required this.failure});
 }
