325e58f0c1a30640ac09112b084ad1544f4595ff
ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-09-17 20:56:35 +0300

    Add  Get PlayList To DI GetIT

--- lib/core/di/get_it.dart
+++ lib/core/di/get_it.dart
@@ -1,4 +1,6 @@
 import 'package:get_it/get_it.dart';
+import 'package:spotify_app/features/home_page/data/repos_impl/get_play_list_repo_impl.dart';
+import 'package:spotify_app/features/home_page/domain/repo/get_play_list_repo.dart';
 import 'package:spotify_app/features/home_page/domain/usecases/get_songs_use_case.dart';
 import 'package:spotify_app/features/login/domain/repo/auth_repo.dart';
 import 'package:spotify_app/features/login/domain/usecases/sign_in_with_email_password_usecase.dart';
@@ -6,6 +8,7 @@ import 'package:spotify_app/features/login/domain/usecases/sign_up_with_email_an
 import 'package:spotify_app/features/login/presentation/cubit/sign_in_cubit/sign_in_cubit.dart';
 import '../../features/home_page/data/repos_impl/get_songs_repo_impl.dart';
 import '../../features/home_page/domain/repo/get_songs_repo.dart';
+import '../../features/home_page/domain/usecases/get_play_list_use_case.dart';
 import '../../features/home_page/presentation/cubit/home_cubit/home_cubit.dart';
 import '../../features/login/data/auth_repo_impl/auth_repo_impl.dart';
 import '../../features/login/presentation/cubit/sign_up_cubit/sign_up_cubit.dart';
@@ -41,8 +44,20 @@ class ServiceLocator {
     //Register GetSongs UseCase
     di.registerLazySingleton<GetSongsUseCase>(
         () => GetSongsUseCase(getSongsRepo: di<GetSongsRepo>()));
+
+    //Register GetPlayList Repo
+    di.registerLazySingleton<GetPlayListRepo>(() => GetPlayListRepoImpl());
+
+    //Register GetPlayList UseCase
+    di.registerLazySingleton<GetPlayListUseCase>(
+        () => GetPlayListUseCase(getPlayListRepo: di<GetPlayListRepo>()));
+
     //Register Home Cubit
     di.registerFactory<HomeCubit>(
-        () => HomeCubit(getSongsUseCase: di<GetSongsUseCase>()));
+      () => HomeCubit(
+        getSongsUseCase: di<GetSongsUseCase>(),
+        getPlayListUseCase: di<GetPlayListUseCase>(),
+      ),
+    );
   }
 }
